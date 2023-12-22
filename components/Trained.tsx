618048516674151eccdf15a1ecfd1da4221a4424
Promtech <uwagboepromise5@gmail.com>
2023-12-22 22:24:40 +0100

    Latest update

--- components/Trained.tsx
+++ components/Trained.tsx
@@ -17,21 +17,21 @@ const Trained = () => {
           <div className="">
             <div className="text-[14px] mt-2 text-dimgray font-extralight">
             <span className="font-bold text-base">Driver Training:</span> Comprehensive course for driver training and
-              safety. <span className="text-blue-600"><Link className="no-underline" href={"#"}>ctaa.org/pass-online</Link></span>
+              safety. <span className="text-blue-600"><a className="no-underline" href="https://ctaa.org/pass-online" target="_blank" rel="noreferrer">ctaa.org/pass-online</a></span>
             </div>
             <div className="text-[14px] mt-2 text-dimgray font-extralight">
-              <span className="font-bold text-base">CPR/First Aid:</span> Certification for emergency readiness, <span className="text-blue-600"><Link className="no-underline" href={"#"}>redcross.org/take-a-class{" "}</Link></span> 
+              <span className="font-bold text-base">CPR/First Aid:</span> Certification for emergency readiness, <span className="text-blue-600"><a className="no-underline" href="https://www.redcross.org/take-a-class" target="_blank" rel="noreferrer">redcross.org/take-a-class</a></span> 
                
             </div>
             <div className="text-[14px] mt-2 text-dimgray font-extralight">
-              <span className="font-bold text-base">Defensive Driving:</span> Skills development for accident prevention. <span className="text-blue-600"><Link className="no-underline" href={"#"}>nsc.org{" "}</Link></span>
+              <span className="font-bold text-base">Defensive Driving:</span> Skills development for accident prevention. <span className="text-blue-600"><a className="no-underline" href="https://www.nsc.org/" target="_blank" rel="noreferrer">nsc.org</a></span>
               
             </div>
             <div className="text-[14px] mt-2 text-dimgray font-extralight mr-4">
               <span className="font-black text-base ">Drug, Alcohol, and Background Check:</span> It is of extreme importance
               to us at Mavride to ensure the trustworthiness and professionalism
               of our staff before employment and randomly while still employed
-              with us. <span className="text-blue-600"><Link className="no-underline" href={"#"}>occusscreen.com</Link></span>, <span className="text-blue-600"><Link className="no-underline" href={"#"}>c-dat.net{" "}</Link></span>
+              with us. <span className="text-blue-600"><a className="no-underline" href="http://occusscreen.com/" target="_blank" rel="noreferrer">occusscreen.com</a></span>, <span className="text-blue-600"><a className="no-underline" href="https://c-dat.net/" target="_blank" rel="noreferrer">c-dat.net{" "}</a></span>
             </div>
           </div>
           <Link href={"/join-us"}>
